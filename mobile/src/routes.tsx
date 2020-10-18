ad931cff04ba005d34b4a42e9c42a705feb73e3b
patrick <patrick@email.com>
2020-10-18 04:18:58 -0300

    finalização do app

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -8,10 +8,15 @@ const { Navigator, Screen } = createStackNavigator();
 import OrphanageMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanagesDetails';
 
+import OrphanageMapPositon from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+
+import Header from './components/Header';
+
 export default function Routers() {
     return (
         <NavigationContainer>
-            <Navigator screenOptions={{ headerShown: false }}>
+            <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5' } }}>
                 <Screen 
                     name="OrphanagesMap" 
                     component={OrphanageMap} 
@@ -20,6 +25,28 @@ export default function Routers() {
                 <Screen 
                     name="OrphanageDetails" 
                     component={OrphanageDetails} 
+                    options={{
+                        headerShown: true,
+                        header: () => <Header showCancel={false} title="Orfanato" />
+                    }}
+                />
+
+                <Screen 
+                    name="OrphanageMapPosition" 
+                    component={OrphanageMapPositon} 
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Selecione no mapa" />
+                    }}
+                />
+
+                <Screen 
+                    name="OrphanageData" 
+                    component={OrphanageData} 
+                    options={{
+                        headerShown: true,
+                        header: () => <Header title="Informe os dados" />
+                    }}
                 />
             </Navigator>
         </NavigationContainer>
